efb2e1fb8e3831c1f602a93632c6cc71af05aff4
Javier García <javier.garcia.olmeda@gmail.com>
2025-09-29 15:43:36 +0200

    terraform init with another provider

--- tf/provider.tf
+++ tf/provider.tf
@@ -8,7 +8,12 @@ terraform {
       source  = "DataDog/datadog"
       version = "~> 3.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
+  required_version = ">= 1.0"
 }
 
 provider "aws" {
