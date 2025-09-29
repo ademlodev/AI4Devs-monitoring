0e978d34c1abc468d05f9d1cada0455520e3f59e
Javier García <javier.garcia.olmeda@gmail.com>
2025-09-29 11:37:20 +0200

    try to deploy and monitoring with datadog

--- tf/scripts/frontend_user_data.sh
+++ tf/scripts/frontend_user_data.sh
@@ -18,3 +18,15 @@ sudo docker run -d -p 3000:3000 lti-frontend
 
 # Timestamp to force update
 echo "Timestamp: ${timestamp}"
+
+
+# Instalar el agente de Datadog
+DD_API_KEY=${datadog_api_key} DD_SITE="us5.datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+
+# Configurar tags para el agente
+echo "tags:" >> /etc/datadog-agent/datadog.yaml
+echo "  - service:frontend" >> /etc/datadog-agent/datadog.yaml
+echo "  - env:production" >> /etc/datadog-agent/datadog.yaml
+
+# Reiniciar el agente
+sudo service datadog-agent restart
